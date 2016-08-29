c5c42b0dab6940b752ec8beacd331258093ad5ff
hany elbana <ebnelbana@hotmail.com>
2016-08-29 12:00:19 +0300

    make it better

--- test/models/history_score_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HistoryScoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/history_trend_chapter_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HistoryTrendChapterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
